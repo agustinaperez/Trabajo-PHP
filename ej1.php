29d5d5cbf11fd79d83451b6ea92f5291398ea636
agustinaperez <83672439+agustinaperez@users.noreply.github.com>
2021-05-28 00:59:23 -0300

    Add files via upload

---
+++ ej1.php
@@ -0,0 +1,14 @@
+<?php
+
+$Nombre = $_POST["ej1.html"];
+$nombre = $_POST["ej1.html"];
+
+if ($Nombre= "SEPTIMO" & $nombre= "Murialdo12") {
+	echo "Has entrado al sistema"
+ }
+else if ( $Nombre== "SEPTIMO" & $nombre== "Murialdo12")
+{
+  echo "Error";
+}
+
+?>
